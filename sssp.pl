2c1a03b9648e5d17c37ee79338640cd8fd49add3
andreagara23 <andreagara23@gmail.com>
2026-02-18 18:10:34 +0100

    Update sssp.pl

--- sssp.pl
+++ sssp.pl
@@ -31,12 +31,12 @@ new_arc(G, U, V, Weight) :- assert(arc(G, U, V, Weight)), !.
 
 new_arc(G, U, V) :- new_arc(G, U, V, 1), !.
 
-arcs(G, []).
-arcs(G, [H|T]) :- arc(G, H), arcs(G, T).
+arcs(G, L) :- findall(arc(G, U, V, K), arc(G, U, V, K), L).
 
-neighbors(G, V, V).
-neighbors(G, V, [H|T]) :- vertex(G, V), arc(G, V, H, K), neighbors(G, V, T).
-neighbors(G, V, [V|T]) :- vertex(G, V), neighbors(G, V, T).
+neighbors(G, V, Ln) :-
+    vertex(G, V),
+    arcs(G, La),
+    findall(N, member(arc(G,V,N,_), La), Ln).
 
 list_arcs(G) :- listing(arc(G, U, V, J)).
 
@@ -60,7 +60,8 @@ not_empty(H) :- \+ empty(H).
 heapify(H, P) :- 		%caso heapify per nodo foglia
 	Ps is P*2, 
 	\+ heap_entry(H, Ps, _, _),
-	Pd is Ps + 1, \+ heap_entry(H, Pd, _, _).
+	Pd is Ps + 1, 
+	\+ heap_entry(H, Pd, _, _).
 	
 heapify(H, P) :- 		%caso heapify per nodo con solo figlio sinistro senza scambio
 	Ps is P*2, 
@@ -68,19 +69,23 @@ heapify(H, P) :- 		%caso heapify per nodo con solo figlio sinistro senza scambio
 	Pd is Ps + 1, 
 	\+ heap_entry(H, Pd, Kd, Vd), 
 	heap_entry(H, P, K, V), 
-	heapify(H, Ps), K =< Ks.
+	heapify(H, Ps), 
+	heap_entry(H, Ps, Ksn, Vsn),
+	K =< Ksn.
 	
-heapify(H, P) :- %caso heapify per nodo con solo figlio sinistro con scambio
+heapify(H, P) :- 		%caso heapify per nodo con solo figlio sinistro con scambio
 	Ps is P*2, 
 	heap_entry(H, Ps, Ks, Vs), 
 	Pd is Ps + 1, 
 	\+ heap_entry(H, Pd, Kd, Vd), 
-	heap_entry(H, P, K, V), 
-	heapify(H, Ps), 
-	K > Ks, 
-	swap(H, K, Ks).
+	heap_entry(H, P, K, V),
+	heap_entry(H, Ps, Ksn, Vsn),
+	K > Ksn, 
+	swap(H, K, Ksn),
+	heapify(H, Ps).
 	
-heapify(H, P) :- 		%caso heapify per nodo con entrambi i figli senza scambio
+heapify(H, P) :- %caso heapify per nodo con entrambi i figli senza scambio
+	
 	Ps is P*2, 
 	heap_entry(H, Ps, Ks, Vs), 
 	Pd is Ps + 1, 
@@ -88,8 +93,10 @@ heapify(H, P) :- 		%caso heapify per nodo con entrambi i figli senza scambio
 	heap_entry(H, P, K, V), 
 	heapify(H, Ps), 
 	heapify(H, Pd), 
-	K =< Ks, 
-	K =< Kd.
+	heap_entry(H, Ps, Ksn, Vsn),
+	heap_entry(H, Pd, Kdn, Vdn),
+	K =< Ksn, 
+	K =< Kdn.
 	
 heapify(H, P) :- 		%caso heapify per nodo con entrmabi i figli e scambio sul figlio sinistro
 	Ps is P*2, 
@@ -97,23 +104,57 @@ heapify(H, P) :- 		%caso heapify per nodo con entrmabi i figli e scambio sul fig
 	Pd is Ps + 1, 
 	heap_entry(H, Pd, Kd, Vd), 
 	heap_entry(H, P, K, V), 
+	heap_entry(H, Ps, Ksn, Vsn),
+	heap_entry(H, Pd, Kdn, Vdn),
+	K > Ksn, 
+	swap(H, K, Ksn), 
+	K =< Kdn,
 	heapify(H, Ps), 
-	heapify(H, Pd), 
-	K > Ks, 
-	swap(H, K, Ks), 
-	K =< Kd.
+	heapify(H, Pd).
 	
 heapify(H, P) :- 		%caso heapify per nodo con entrmabi i figli e scambio sul figlio destro
 	Ps is P*2, 
 	heap_entry(H, Ps, Ks, Vs), 
 	Pd is Ps + 1, 
 	heap_entry(H, Pd, Kd, Vd), 
-	heap_entry(H, P, K, V), 
+	heap_entry(H, P, K, V),
+	heap_entry(H, Ps, Ksn, Vsn),
+	heap_entry(H, Pd, Kdn, Vdn),
+	K =< Ksn, 
+	K > Kdn, 
+	swap(H, K, Kdn),
 	heapify(H, Ps), 
-	heapify(H, Pd), 
-	K =< Ks, 
-	K > Kd, 
-	swap(H, K, Kd).
+	heapify(H, Pd).
+	
+heapify(H, P) :- 
+	Ps is P*2, 
+	heap_entry(H, Ps, Ks, Vs), 
+	Pd is Ps + 1, 
+	heap_entry(H, Pd, Kd, Vd), 
+	heap_entry(H, P, K, V),
+	heap_entry(H, Ps, Ksn, Vsn),
+	heap_entry(H, Pd, Kdn, Vdn),
+	K > Ksn,
+	K > Kdn,
+	Kdn < Ksn,
+	swap(H, K, Kdn),
+	heapify(H, Ps), 
+	heapify(H, Pd).
+	
+heapify(H, P) :- 
+	Ps is P*2, 
+	heap_entry(H, Ps, Ks, Vs), 
+	Pd is Ps + 1, 
+	heap_entry(H, Pd, Kd, Vd), 
+	heap_entry(H, P, K, V),
+	heap_entry(H, Ps, Ksn, Vsn),
+	heap_entry(H, Pd, Kdn, Vdn),
+	K > Ksn,
+	K > Kdn, 
+	Ksn < Kdn,
+	swap(H, K, Ksn),
+	heapify(H, Ps), 
+	heapify(H, Pd).
 
 insert(H, K, V) :- 
 	heap_size(H, S), 
@@ -129,25 +170,26 @@ head(H, K, V) :- heap_entry(H,1,K,V).
 
 extract(H, K, V) :- 
 	heap_entry(H, P, K, V), 
+	P = 1,
 	heap_size(H, S), 
 	S1 is S-1, 
 	retract(heap(H, S)), 
 	assert(heap(H, S1)), 
 	P1 is P+1, 
 	retract(heap_entry(H, P, K, V)), 
-	adapt(H, P1), 
-	heapify(H, 1), 
-	heapify(H, 1).
+	\+ adapt(H, P1),
+	heapify(H, 1),
+	!.
 
 adapt(H, P) :- 
 	heap_size(H, S), 
 	heap_entry(H, P, K, V), 
+	P1 is P-1,
+	P2 is P+1,
+	P1>0,
 	retract(heap_entry(H, P, K, V)), 
-	P1 is P-1, 
-	assert(heap_entry(H, P1, K, V)), 
-	P2 is P + 1, 
-	adapt(H, P2), 
-	heapify(H, 1).
+	assert(heap_entry(H, P1, K, V)),  
+	adapt(H, P2).
 
 modify_key(H, NewKey, OldKey, V) :- 
 	retract(heap_entry(H, P, OldKey, V)), 
@@ -157,18 +199,94 @@ swap(H, K1, K2) :-
 	retract(heap_entry(H, P1, K1, V1)), 
 	retract(heap_entry(H, P2, K2, V2)), 
 	assert(heap_entry(H, P2, K1, V1)), 
-	assert(heap_entry(H, P1, K2, V2)), 
-	heapify(H, 1).
+	assert(heap_entry(H, P1, K2, V2)).
 
 delete_heap(H) :- retractall(heap_entry(H, _, _, _)), retract(heap(H, _)).
 
-?- new_heap(heap).
-?- insert(heap, 10, a).
-?- insert(heap, 20, b).
-?- insert(heap, 7, c).
-?- insert(heap, 5, d).
-?- insert(heap, 1, e).
-?- insert(heap, 15, f).
-?- insert(heap, 27, g).
-?- insert(heap, 12, h).
-?- insert(heap, 4, i).
+:- dynamic distance/3.
+
+:- dynamic visited/2.
+
+:- dynamic previous/3.
+
+
+change_distance(G, V, NewDist) :- 
+	retractall(distance(G, V, _)),
+	assert(distance(G, V; NewDist)).
+
+change_previous(G, V, U) :-
+	retractall(previous(G, V, _)),
+	assert(previous(G, V, U)).
+	
+dijkstra_sssp(G, Source):-
+	new_heap(dijkheap),
+	neighbors(G, S, L1),
+	source_weights(G, S, L1, dijkheap),
+	visit(G, dijkheap),
+	!.
+	
+	
+
+source_weights(G, S, [], _).
+source_weights(G, S, [H|T], Heap):- 
+	arc(G, S, H, K),
+	assert(distance(G, H, K)),
+	insert(Heap, K, distance(G, H, K)),
+	assert(previous(G, H, S)),
+	source_weights(G, S, T, Heap).
+
+visit(G, Heap) :- 
+	empty(Heap).
+	
+visit(G, Heap) :-
+	not_empty(Heap),
+	extract(Heap, K, V),
+	arg(2, V, N),
+	assert(visited(G, N)),
+	neighbors(G, N, Lv),
+	node_weights(G, N, Lv, Heap, K),
+	visit(G, Heap).
+
+node_weights(G, N, [], _, K).	
+	
+node_weights(G, N, [H|T], Heap, Kp):- 
+	\+ visited(G, H),
+	arc(G, N, H, K),
+	Ktot is K + Kp,
+	assert(distance(G, H, Ktot)),
+	assert(previous(G, H, N)),
+	insert(Heap, Ktot, distance(G, H, Ktot)),
+	node_weights(G, S, T, Heap, Kp).	
+	
+node_weights(G, N, [H|T], Heap, Kp):- 
+	visited(G, H),
+	arc(G, N, H, K),
+	Ktot is K + Kp,
+	distance(G, H, Kpre),
+	Kpre > Ktot,
+	change_distance(G, H, Ktot),
+	change_previous(G, H, N),
+	insert(Heap, Ktot, distance(G, H, Ktot)),
+	node_weights(G, S, T, Heap, Kp).	
+	
+node_weights(G, N, [H|T], Heap, Kp):- 
+	visited(G, H),
+	arc(G, N, H, K),
+	Ktot is K + Kp,
+	distance(G, H, Kpre),
+	Kpre< Ktot,
+	node_weights(G, S, T, Heap, Kp).
+
+
+?- new_graph(grafo).
+?- new_vertex(grafo, a).
+?- new_vertex(grafo, b).
+?- new_vertex(grafo, c).
+?- new_vertex(grafo, d).
+?- new_vertex(grafo, e).
+?- new_arc(grafo, a, b, 1).
+?- new_arc(grafo, a, c, 3).
+?- new_arc(grafo, b, d, 3).
+?- new_arc(grafo, c, e, 1).
+?- new_arc(grafo, e, d, 1).
+?- new_arc(grafo, b, e, 5).
